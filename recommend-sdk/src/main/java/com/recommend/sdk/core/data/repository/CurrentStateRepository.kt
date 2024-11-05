7bab3744826d1d7b8c2779874421ed1c1c4ed3cb
Dmitry Kosenko <71468576+Ksnk-dm@users.noreply.github.com>
2024-11-05 10:36:07 +0200

    new-feat

--- recommend-sdk/src/main/java/com/recommend/sdk/core/data/repository/CurrentStateRepository.kt
+++ recommend-sdk/src/main/java/com/recommend/sdk/core/data/repository/CurrentStateRepository.kt
@@ -1,12 +1,19 @@
 package com.recommend.sdk.core.data.repository
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.SharedPreferences
+import android.net.wifi.WifiManager
+import android.os.Build
+import android.provider.Settings
+import android.telephony.TelephonyManager
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.*
 import androidx.datastore.preferences.preferencesDataStore
 import com.recommend.sdk.core.data.model.CurrentState
 import kotlinx.coroutines.flow.first
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 import java.util.*
 
 /**
@@ -20,6 +27,7 @@ class CurrentStateRepository(private val context: Context) {
     companion object {
         const val CURRENT_STATE_PREFERENCES_NAME = "RECOMMEND_CURRENT_STATE"
         const val DEVICE_OLD_SDK_PREFERENCES_KEY = "PREF_UNIQUE_ID"
+        private const val SALT = "salt"
     }
 
     private val Context.recommendCurrentStateDataStore: DataStore<Preferences> by preferencesDataStore(name = CURRENT_STATE_PREFERENCES_NAME)
@@ -40,7 +48,7 @@ class CurrentStateRepository(private val context: Context) {
             val deviceIdFromOldSDK = getDeviceIdFromOldSDK()
 
             val newCurrentState = CurrentState(
-                deviceIdFromOldSDK ?: UUID.randomUUID().toString()
+                deviceIdFromOldSDK ?: getSecretKey(context)
             )
             saveCurrentState(newCurrentState)
             newCurrentState
@@ -91,5 +99,30 @@ class CurrentStateRepository(private val context: Context) {
             DEVICE_OLD_SDK_PREFERENCES_KEY,
             null
         )
+    }
+
+    @SuppressLint("HardwareIds")
+    private fun getSecretKey(context: Context): String {
+        val short = "35" + Build.BOARD.length % 10 + Build.BRAND.length % 10 + Build.CPU_ABI.length % 10 + Build.DEVICE.length % 10 + Build.DISPLAY.length % 10 + Build.HOST.length % 10 + Build.ID.length % 10 + Build.MANUFACTURER.length % 10 + Build.MODEL.length % 10 + Build.PRODUCT.length % 10 + Build.TAGS.length % 10 + Build.TYPE.length % 10 + Build.USER.length % 10 //13 digits
+        val advId = Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
+        val wm = context.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        val macAddress = wm.connectionInfo.macAddress
+        var longID = short + advId + macAddress;
+        longID += SALT + longID.length * SALT.length
+        var m: MessageDigest? = null
+        try {
+            m = MessageDigest.getInstance("MD5")
+        } catch (e: NoSuchAlgorithmException) {
+            e.printStackTrace()
+        }
+        m!!.update(longID.toByteArray(), 0, longID.length)
+        val digest = m.digest()
+        var uniqueID = ""
+        for (i in digest.indices) {
+            val b = 0xFF and digest[i].toInt()
+            if (b <= 0xF) uniqueID += "0"
+            uniqueID += Integer.toHexString(b)
+        }
+        return uniqueID.uppercase(Locale.getDefault())
     }
 }
